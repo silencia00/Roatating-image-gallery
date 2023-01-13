4a3490b288ca438b3d5241cc82b4b558b1f2e1e6
silencia00 <valentinefranklyn00@gmail.com>
2023-01-13 16:40:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const imageGalleryEl = document.querySelector(".image-container");
+const prevEl = document.getElementById("prev")
+const nextEl = document.getElementById("next")
+let x = 0
+let timer;
+
+prevEl.addEventListener("click", () => {
+    x += 45;
+    clearTimeout(timer)
+    updateGallery()
+});
+nextEl.addEventListener("click", () => {
+    x -= 45;
+     clearTimeout(timer)
+    updateGallery()
+});
+
+function updateGallery() {
+    imageGalleryEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer =setTimeout(() =>{
+        x -= 45
+        updateGallery()
+    }, 3000)
+}
+
+updateGallery()
